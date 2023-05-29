51a35514ca50835af11cb725dd5b0f25be50616e
devyansh3 <65131029+devyansh3@users.noreply.github.com>
2023-05-29 17:04:39 +0530

    push commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request,jsonify
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/postJSON', methods = ["POST"])
 def receive_json():
